b82219b1bffc9a4f2fc080bee4e41943516120fe
Francisco Angelo <franciscoangelo.feo@gmail.com>
2021-12-04 23:46:48 -0300

    Created the User model for authentication later

---
+++ src/main/java/br/com/cooperativa/model/Usuario.java
@@ -0,0 +1,82 @@
+package br.com.cooperativa.model;
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
+
+
+@Entity
+public class Usuario  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Campo usuario é obrigatorio.")
+    private String username;
+
+    @NotEmpty(message = "Campo senha é obrigatorio.")
+    private String password;
+
+    @NotEmpty(message = "Campo email é obrigatório")
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id) && username.equals(usuario.username) && password.equals(usuario.password) && email.equals(usuario.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, email);
+    }
+}
