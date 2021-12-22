c1322ded7361249a500f88b05df46e05b139ba1f
Albert Ribeiro <albertribeirov@gmail.com>
2021-12-22 02:56:44 -0300

    Criando consulta para encontrar um cliente por cpf ou cnpj

--- src/main/java/br/com/cooperativa/repository/ClienteRepository.java
+++ src/main/java/br/com/cooperativa/repository/ClienteRepository.java
@@ -2,8 +2,14 @@ package br.com.cooperativa.repository;
 
 import br.com.cooperativa.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    @Query("select * from Cliente c WHERE c.cpf_cnpj = :cpf")
+    Optional<Cliente> findByCpf(String cpf);
 }
