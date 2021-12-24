9cc2fa8d9b76764f6da7c236bd5052ae9b0b27a4
Sadraque Nunes <sadraquenunesmartiniano@gmail.com>
2021-12-23 22:13:39 -0300

    add Swagger OpenAPI for documentation

---
+++ src/main/java/br/com/cooperativa/configuration/SwaggerConfiguration.java
@@ -0,0 +1,26 @@
+package br.com.cooperativa.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cooperativa API")
+                        .version("1.0")
+                        .description("Cooperativa API")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                        .contact(new Contact()
+                                .name("Cooperativa")));
+
+    }
+}
