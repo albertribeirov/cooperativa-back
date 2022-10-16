39a2f3ba117bc2733b259e92ee543881cf2f8231
Albert Ribeiro <albertribeirov@gmail.com>
2022-10-16 17:41:24 -0300

    Configurando TypeUtils

--- src/main/java/br/com/cooperativa/utils/TypeUtils.java
+++ src/main/java/br/com/cooperativa/utils/TypeUtils.java
@@ -1,23 +1,25 @@
 package br.com.cooperativa.utils;
 
+import lombok.experimental.UtilityClass;
 import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@UtilityClass
 public class TypeUtils {
 
-    final private static ModelMapper modelMapper = new ModelMapper();
+    final private ModelMapper modelMapper = new ModelMapper();
 
-    public static <T> T parseToEntity(Object dto, Class<T> entity) {
+    public <T> T parseToEntity(Object dto, Class<T> entity) {
         return modelMapper.map(dto, entity);
     }
 
-    public static <T> T parseToDTO(Object entity, Class<T> dto) {
+    public <T> T parseToDTO(Object entity, Class<T> dto) {
         return modelMapper.map(entity, dto);
     }
 
-    public static <T> List<T> parseToDTO(List<?> entity, Class<T> dto) {
+    public <T> List<T> parseToDTO(List<?> entity, Class<T> dto) {
         List<T> list = new ArrayList<>();
 
         entity.forEach(o -> list.add(modelMapper.map(o, dto)));
@@ -25,7 +27,7 @@ public class TypeUtils {
         return list;
     }
 
-    public static <T> List<T> parseToEntity(List<?> dto, Class<T> entity) {
+    public <T> List<T> parseToEntity(List<?> dto, Class<T> entity) {
         List<T> list = new ArrayList<>();
 
         dto.forEach(o -> list.add(modelMapper.map(o, entity)));
