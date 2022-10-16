aac001362bc2c8e0c45754e42c1498939aa9fcd4
Albert Ribeiro <albertribeirov@gmail.com>
2022-10-16 17:44:44 -0300

    Excluindo DML de testes

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO endereco (bairro, cep, cidade, create_time, rua_com_numero, update_time)
-VALUES ('Janga', '53110120', 'Paulista', current_timestamp, 'Avenida Amazonas, 1020', current_timestamp);
-
-
-INSERT INTO endereco (bairro, cep, cidade, create_time, rua_com_numero, update_time)
-VALUES ('Universitário', '53110120', 'Caruaru', current_timestamp, 'Avenida Portugal, 102', current_timestamp);
